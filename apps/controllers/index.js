a813180fa009a00fdb48e3bf2841d16046553881
TranThanhIT <tranthanh.it.95@gmail.com>
2017-06-13 20:23:25 +0700

    Update feature login, register

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -19,39 +19,61 @@ router.get("/login", function(req, res) {
 router.post("/login", function(req, res) {
     var params = req.body;
     console.log(params);
-    if (params.email.trim().length == 0) {
-        res.render("login", {
-            data: {
-                error: "Please enter an email!"
-            }
-        });
+    console.log(params.remember == '1');
+    console.log(req.session.cookie);
+    if (req.session.cookie) {
+        res.redirect("/homepage");
     } else {
-        var data = user_md.getUserByEmail(params.email);
-        if (data) {
-            data.then(function(users) {
-                console.log(JSON.stringify(users));
-                var user = users[0];
-                var status = helper.compare_password(params.password, user.password);
-                if (!status) {
-                    res.render("login", {
-                        data: {
-                            error: "Password Wrong!"
-                        }
-                    });
-                } else {
-                    req.session.user = user;
-                    console.log(req.session.user);
-                    res.redirect("/homepage");
+        if (params.email.trim().length == 0) {
+            res.render("login", {
+                data: {
+                    error: "Please enter an email!"
                 }
             });
         } else {
-            res.render("/login", {
-                data: {
-                    error: "User not exists!"
-                }
-            });
+            var data = user_md.getUserByEmail(params.email);
+            if (data) {
+                data.then(function(users) {
+                    // console.log(JSON.stringify(users));
+                    if (users.length > 0) {
+                        var user = users[0];
+                        var status = helper.compare_password(params.password, user.password);
+                        if (!status) {
+                            res.render("login", {
+                                data: {
+                                    error: "Password Wrong!"
+                                }
+                            });
+                        } else {
+                            if (params.remember == '1') {
+                                console.log('asdsdas');
+                                req.session.cookie.maxAge = 2592000000; // 30*24*60*60*1000 Rememeber 'me' for 30 days
+                                console.log(req.session.cookie);
+                            } else {
+                                req.session.cookie.expires = false;
+                                req.session.user = user;
+                                console.log(req.session.user);
+                                res.redirect("/homepage");
+                            }
+                        }
+                    } else {
+                        res.render("login", {
+                            data: {
+                                error: "Username not found!!"
+                            }
+                        });
+                    }
+                });
+            } else {
+                res.render("login", {
+                    data: {
+                        error: "User not exists!"
+                    }
+                });
+            }
         }
     }
+
 });
 
 // Router register form
@@ -61,6 +83,30 @@ router.get("/register", function(req, res) {
 
 // Function when user register and check.
 router.post("/register", function(req, res) {
+
+    /*
+    // Check captcha.
+    // g-recaptcha-response is the key that browser will generate upon form submit.
+    // if its blank or null means user has not selected the captcha, so return the error.
+    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        return res.json({ "responseCode": 1, "responseDesc": "Please select captcha" });
+    }
+    // Put your secret key here.
+    var secretKey = "6Lc4GSUUAAAAACY8_1G3-ACb7cXg9YvcW7pMSmsd";
+    // req.connection.remoteAddress will provide IP address of connected user.
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    // Hitting GET request to the URL, Google will respond with success or error scenario.
+    request(verificationUrl, function(error, response, body) {
+        body = JSON.parse(body);
+        // Success will be true or false depending upon captcha validation.
+        if (body.success !== undefined && !body.success) {
+            return res.json({ "responseCode": 1, "responseDesc": "Failed captcha verification" });
+        }
+        res.json({ "responseCode": 0, "responseDesc": "Sucess" });
+    });
+
+    //---------------- */
+
     var user = req.body;
     console.log(user);
     if (user.email.trim().length == 0) {
@@ -77,6 +123,7 @@ router.post("/register", function(req, res) {
             }
         });
     }
+
     // Insert DB.
     var password = helper.hash_password(user.password);
     // user model
@@ -96,6 +143,21 @@ router.post("/register", function(req, res) {
             }
         });
     });
+});
+
+// This will handle forgot password requests.
+router.get("/forgot", function(req, res) {
+    res.render("forgotpassword");
+});
+
+// This will handle forgot password requests.
+router.get("/homepage", function(req, res) {
+    res.render("homepage");
+});
+
+// This will handle 404 requests.
+router.get("*", function(req, res) {
+    res.status(404).send("404");
 });
 
 module.exports = router;
